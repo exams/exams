58bbe40d351ac75d9d03809566fb677a89431693
Domines <719480072@qq.com>
2018-10-22 19:18:23 +0800

    Update HeaderCustom.jsx

--- src/components/HeaderCustom.jsx
+++ src/components/HeaderCustom.jsx
@@ -10,6 +10,7 @@ import avater from '../style/imgs/b1.jpg';
 import SiderCustom from './SiderCustom';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { Row, Col } from 'antd';
 const { Header } = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
@@ -78,51 +79,40 @@ class HeaderCustom extends Component {
     render() {
         const { responsive, path } = this.props;
         return (
-            <Header style={{ background: '#fff', padding: 0, height: 65 }} className="custom-theme" >
-                {
-                    responsive.data.isMobile ? (
-                        <Popover content={<SiderCustom path={path} popoverHide={this.popoverHide} />} trigger="click" placement="bottomLeft" visible={this.state.visible} onVisibleChange={this.handleVisibleChange}>
-                            <Icon type="bars" className="trigger custom-trigger" />
-                        </Popover>
-                    ) : (
-                        <Icon
-                            className="trigger custom-trigger"
-                            type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
-                            onClick={this.props.toggle}
-                        />
-                    )
-                }
-                <Menu
-                    mode="horizontal"
-                    style={{ lineHeight: '64px', float: 'right' }}
-                    onClick={this.menuClick}
-                >
-                    <Menu.Item key="full" onClick={this.screenFull} >
-                        <Icon type="arrows-alt" onClick={this.screenFull} />
-                    </Menu.Item>
-                    <Menu.Item key="1">
-                        <Badge count={25} overflowCount={10} style={{marginLeft: 10}}>
-                            <Icon type="notification" />
-                        </Badge>
-                    </Menu.Item>
-                    <SubMenu title={<span className="avatar"><img src={avater} alt="头像" /><i className="on bottom b-white" /></span>}>
-                        <MenuItemGroup title="用户中心">
-                            <Menu.Item key="setting:1">你好 - {this.props.user.userName}</Menu.Item>
-                            <Menu.Item key="setting:2">个人信息</Menu.Item>
-                            <Menu.Item key="logout"><span onClick={this.logout}>退出登录</span></Menu.Item>
-                        </MenuItemGroup>
-                        <MenuItemGroup title="设置中心">
-                            <Menu.Item key="setting:3">个人设置</Menu.Item>
-                            <Menu.Item key="setting:4">系统设置</Menu.Item>
-                        </MenuItemGroup>
-                    </SubMenu>
-                </Menu>
-                <style>{`
-                    .ant-menu-submenu-horizontal > .ant-menu {
-                        width: 120px;
-                        left: -40px;
-                    }
-                `}</style>
+            <Header className="custom-theme" >
+                <Row>
+                    <Col span={18}>
+                        <SiderCustom></SiderCustom>
+                    </Col>
+                    <Col span={6}>
+                        <Menu
+                            mode="horizontal"
+                            theme="dark"
+                            style={{float: 'right', height:'60px', lineHeight: '60px' }}
+                            onClick={this.menuClick}
+                        >
+                            <Menu.Item key="full" onClick={this.screenFull} >
+                                <Icon type="arrows-alt" onClick={this.screenFull} />
+                            </Menu.Item>
+                            <Menu.Item key="1">
+                                <Badge count={25} overflowCount={10} style={{marginLeft: 10}}>
+                                    <Icon type="notification" />
+                                </Badge>
+                            </Menu.Item>
+                            <SubMenu title={<span className="avatar"><img src={avater} alt="头像" /><i className="on bottom b-white" /></span>}>
+                                <MenuItemGroup title="用户中心">
+                                    <Menu.Item key="setting:1">你好 - {this.props.user.userName}</Menu.Item>
+                                    <Menu.Item key="setting:2">个人信息</Menu.Item>
+                                    <Menu.Item key="logout"><span onClick={this.logout}>退出登录</span></Menu.Item>
+                                </MenuItemGroup>
+                                <MenuItemGroup title="设置中心">
+                                    <Menu.Item key="setting:3">个人设置</Menu.Item>
+                                    <Menu.Item key="setting:4">系统设置</Menu.Item>
+                                </MenuItemGroup>
+                            </SubMenu>
+                        </Menu>
+                    </Col>
+                </Row>
             </Header>
         )
     }
